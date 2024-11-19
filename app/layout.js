2aeaf61868cde5ad9c3f86b0632d14fe8beed7ca
berke <cydex201209@gmail.com>
2024-11-19 06:01:40 +0300

    add theming

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
 
 export const metadata = {
@@ -9,7 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	)
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import { ModeToggle } from "@/components/ModeToggle"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
 	return (
 		<div className="">
 			<h1>test</h1>
+			<Button variant="outline">test</Button>
+			<ModeToggle />
 		</div>
 	)
 }
